c7d1927332722c6806eb76be71dd4dc9e454767b
Pallab Majumdar <kitkatpallab@gmail.com>
2023-11-13 23:28:48 +0600

    user profile is done

--- src/components/pages/profile/profilePicture.tsx
+++ src/components/pages/profile/profilePicture.tsx
@@ -2,9 +2,9 @@ import useGetProfile from '../../../hooks/user/useGetProfile';
 import ProfilePictureOrganism from '../../organism/profile/profilePicture';
 
 const ProfilePicturePage = () => {
-  const { loading, data, error } = useGetProfile();
+  const { loading, data } = useGetProfile();
 
-  return <ProfilePictureOrganism data={data.data} loading={loading} />;
+  return <ProfilePictureOrganism data={data?.data} loading={loading} />;
 };
 
 export default ProfilePicturePage;
